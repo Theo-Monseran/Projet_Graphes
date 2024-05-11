7d1c5087a6ba9d3e4dca3fa4f48cfee59b64be04
Theo Monseran <tmonseran@enseirb-matmeca.fr>
2024-05-11 19:36:27 +0200

    Added Position class in part 2 and modified code for part 3

--- part2.py
+++ part2.py
@@ -1,22 +1,38 @@
+class Position:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    @staticmethod
+    def string_to_pos(string):
+        x_y = string[1:len(string)-2].split(",")
+        return Position(int(x_y[0]), int(x_y[1]))
+
+
 def lire_fichier_graphe(N, nom_fichier):
-    graphe = {'villages': [], 'drones': [], 'deplacements': []}
+    villages = []
+    drones = []
+    deplacements = []
     with open(nom_fichier, 'r') as f:
         for ligne in f:
             if ligne:
                 partie = ligne.split(" : ")
                 entite = partie[0]
                 if (entite != 'X'):
-                    position = [int(x) for x in partie[1].strip("\n").strip("(").strip(")").split(",")]
+                    position = Position(partie[1].strip("\n").strip("(").strip(")").split(",")[0], 
+                                        partie[1].strip("\n").strip("(").strip(")").split(",")[1])
                 if entite == 'D':
-                    graphe['drones'].append(position)
+                    drones.append(position)
                 elif entite.isdigit():
-                    graphe['villages'].append(position)
+                    villages.append(position)
                 elif entite == 'E':
-                    graphe['deplacements'].append(position)
-    return graphe
+                    deplacements.append(position)
+    return villages, drones, deplacements
 
 
 N = 3
 nom_fichier = 'data.txt'
-graphe = lire_fichier_graphe(N, nom_fichier)
-print(graphe)
+villages, drones, deplacements = lire_fichier_graphe(N, nom_fichier)
+print(villages[1].x)
+print(drones[0].x)
+print(drones[0].y)

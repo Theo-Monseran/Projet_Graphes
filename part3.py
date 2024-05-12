79344888e064dc9e08ca9cc4ec6ac3d92392d75d
Theo Monseran <tmonseran@enseirb-matmeca.fr>
2024-05-12 12:31:06 +0200

    Added tsp_glouton in part3

--- part3.py
+++ part3.py
@@ -1,10 +1,33 @@
 import numpy as np
+from part2 import *
 
 
 def distance(p1, p2):
     return np.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
 
 
-# def parcours_optimal(village, drones, deplacements):
-#     visited = np.zeros(len[village])
-#     pass
+# return list of villages from nearest to drones to furthest
+# (0 is the one the drone is currently on)
+def get_nearest(villages, drone, deplacements):
+    # TODO
+    pass
+
+
+def tsp_glouton(villages, drones, deplacements):
+    visited = []
+    i = 0
+    damage = [0 for i in range(len(villages))]
+    for drone in drones:
+        visited.append([drone.x, drone.y])
+    while len(visited) < len(villages):
+        for drone in drones:
+            nearest_villages = get_nearest(villages, drone, deplacements)
+            for village in nearest_villages:
+                if village not in visited:
+                    drone.x = village.x
+                    drone.y = village.y
+        for v in range(len(villages)):
+            if villages[v] not in visited:
+                damage[v] += 1
+        i += 1
+    return i, damage
